9ce8cab7f5cee3c83fab2188d4ae2ce71d846d0f
August2111 <Info@FlapsOnline.de>
2021-02-08 11:11:59 +0100

    MSVC util/Compiler.h for VS
    a lot of preprocessor and compiler settings for VisualStudio

--- src/util/Compiler.h
+++ src/util/Compiler.h
@@ -24,6 +24,67 @@ Copyright_License {
 #ifndef COMPILER_H
 #define COMPILER_H
 
+#ifdef _MSC_VER
+
+#include <io.h>
+#include   "corecrt_math_defines.h"
+
+#define STRING2(x) #x
+#define STRING(x) STRING2(x)
+
+#define CLANG_OR_GCC_VERSION(x, y)  0
+#define GCC_CHECK_VERSION(x, y)     0
+#define GCC_OLDER_THAN(x, y)        1  // This isn't a (new!) GCC
+#define CLANG_CHECK_VERSION(x, y)   0
+
+#define DT_UNDERLINE 0  // gibt es in WinUser.h nicht!
+
+#define __attribute__(x)
+
+#define gcc_const
+#define gcc_deprecated
+#define gcc_may_alias
+#define gcc_malloc
+#define gcc_noreturn
+#define gcc_packed
+#define gcc_printf(a,b)
+// aug:format(printf, a, b)
+#define gcc_pure  //  aug:
+#define gcc_sentinel    //  aug: sentinel
+#define gcc_unused   //  aug: unused
+#define gcc_warn_unused_result  //  aug: warn_unused_result
+
+#define gcc_nonnull(...)
+// __assume(__VA_ARGS__ != nullptr)
+#define gcc_nonnull_all   // __assume(!nullptr)
+#define gcc_returns_nonnull // returns_nonnull
+
+#define gcc_likely(x)  (x)
+//  aug: __builtin_expect (!!(x), 1)
+#define gcc_unlikely(x)  (x)
+//  aug: __builtin_expect (!!(x), 0)
+
+#define gcc_aligned(n)
+//  aug: aligned(n)
+
+#define gcc_visibility_hidden  //  aug: visibility("hidden")
+#define gcc_visibility_default  //  aug: visibility("default")
+
+#define gcc_always_inline
+// inline  // wird dann mehrfach verwendet... //  aug: always_inline
+
+
+// unuse deprecated functions:
+#define strdup(a)             _strdup(a)
+#define wcsdup(a)             _wcsdup(a)
+
+// TODO(August2111): where is this defined (on GCC)?
+# ifndef ssize_t
+  typedef size_t   ssize_t;
+# endif  // ssize_t
+
+#else  // _MSC_VER
+
 #define GCC_MAKE_VERSION(major, minor, patchlevel) ((major) * 10000 + (minor) * 100 + patchlevel)
 
 #ifdef __GNUC__
@@ -69,7 +130,7 @@ Copyright_License {
 #  if GCC_OLDER_THAN(10,0)
 #    error Sorry, your gcc version is too old.  You need at least version 10.
 #  endif
-#else
+#else 
 #  warning Untested compiler.  Use at your own risk!
 #endif
 
@@ -140,6 +201,8 @@ Copyright_License {
 #define gcc_always_inline inline
 
 #endif
+
+#endif  // _MSC_VER
 
 #if CLANG_OR_GCC_VERSION(4,3)
 
