8032710db1000733d03095e3e2bad2a02cbdf1e6
August2111 <Info@FlapsOnline.de>
2021-10-04 21:42:04 +0200

    CMAKE CMAKE CMAKE CMAKE CMAKE CMAKE CMAKE CMAKE CMAKE CMAKE CMAKE Add cmake files to the project ==============================
    add Boost compiling to project (with python thirdparty.py)
    
    Modify  cmake for a integration of the header files in the project
    
    cmake: include c-ares lib
    
    cmake: reorganize the jasper inclusion
    
    CMake Scripting changes to bring the system 'to fly'
    
    3rd party changes:
    * cleanup CMakeLists.txt
    * make the inside and outside setting of ZZIP and JASPER more transparent
    
    generate the language files (.mo and .mo.c) with a style that works in both evironments make/gcc,clang and cmake/msvc
    use python instead of perl for script files
    
    cmake main file cleanup
    
    CMake cleanup BOUND_NOEXCEPT
    
    CMake: path variable to python
    
    CMake: add Jasper to cmake
    
    CMake Cmake-Changes for 7.0
    
    hiddened files
    
    CMake Change CMake files
    
    CMake Change CMake files
    
    CMake: switch to msvc:
    
    CMake remove BOUND_EXCEPT from CMakeLists.txt
    
    CMake: CMakeSource.cmake
    Multi.cpp to Mult.cxx
    HTTP folder to http
    
    CMake redefined GNU_PURE for Non-MSVC
    
    CMake add GIT_COMMIT_ID
    
    CMake better call convention for Build
    
    CMake: Reorder the net-lib
    
    CMake update io lib
    
    CMake add CMakeSource.cmake internally
    
    CMake add version string from VERSION.txt to xcsoar
    
    CMake: add subdirectory Cloud
    
    CMake: add FileHandling
    
    CMake: add the UploadManager to cmake too
    
    CMake: add lua scripting in cmake too
    
    CMake include the project files inside Projection/CMakeLists.txt
    
    remove the import in the 'overall' CMakeSource.cmake
    
    CMake cleanup the overall CMakeSource.cmake
    
    CMake disable warning C5030: gnu::pure, gnu::const
    
    CMake Cloud/CMakeSource.cmake (add DownloadTask.cpp)
    
    CMake test/CMakeSource (adding test!)
    
    Creating MathTables

---
+++ src/Cloud/CMakeSource.cmake
@@ -0,0 +1,10 @@
+set(Cloud_SOURCES
+        Cloud/weglide/WeGlideSettings.cpp
+        Cloud/weglide/UploadFlight.cpp
+)
+
+set(SCRIPT_FILES
+    CMakeSource.cmake
+)
+
+
---
+++ src/time/CMakeSource.cmake
@@ -0,0 +1,13 @@
+set(time_SOURCES
+        time/BrokenDate.cpp
+        time/BrokenDateTime.cpp
+        time/BrokenTime.cpp
+        time/Convert.cxx
+        time/DeltaTime.cpp
+        time/LocalTime.cpp
+        time/WrapClock.cpp
+        time/Zone.cxx
+)
+
+set(SCRIPT_FILES CMakeSource.cmake)
+
