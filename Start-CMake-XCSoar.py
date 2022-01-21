af357afb23be27c4032adb667396e73dc8c3669c
August2111 <Info@FlapsOnline.de>
2022-01-21 23:48:37 +0100

    Python files for CMake

---
+++ Start-CMake-XCSoar.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os, sys
+from CMakeXCSoar import create_xcsoar
+class ComputerDirectories(object):
+    def __init__(self, name, directories):
+        self.name = name
+        self.project_dir = directories["project_dir"]
+        self.third_party_dir = directories["third_party_dir"]
+        self.binary_dir = directories["binary_dir"]
+        self.program_dir = directories["program_dir"]
+        self.link_libs = directories["link_libs"]
+
+
+if len(sys.argv) > 1:
+  project = sys.argv[1]
+else:
+  project = "XCSoarAug"
+if len(sys.argv) > 2:
+  toolchain = sys.argv[2]
+else:
+  toolchain = "msvc2019"
+
+print('Project Name = ', project)
+print('Toolchain    = ', toolchain)
+
+os.system("pause")
+
+
+if sys.platform.startswith('win'):
+    if not toolchain in ['mgw73', 'mgw103', 'mgw112', 'ninja', 'clang10', 'msvc2019', 'msvc2022']:
+        toolchain = 'mgw112'
+        # toolchain = 'mgw73'
+        # toolchain = 'mgw103'
+        # toolchain = 'mgw112'
+        # toolchain = 'ninja'
+        # toolchain = 
+        # toolchain = 'msvc2019'
+else:
+    if not toolchain in ['unix']:
+        toolchain = 'unix'
+
+arguments = []
+arguments.append('XCSoarAug')   # project_name
+# arguments.append('master')      # branch
+arguments.append('weglide_msvc_new')      # branch
+arguments.append(toolchain)     # build-toolchain
+# arguments.append('Arg2')
+# arguments.append('Arg3')
+# arguments.append('Arg4')
+
+print('Jetzt gehts los: ', arguments)
+create_xcsoar(arguments)
+
+
+## batch: set SOURCE_DIR=%CD%
+## batch: set BINARY_DIR=D:\Projects\Binaries
+## batch: set TARGET_PLATFORM=64
+## batch: set THIRD_PARTY=D:/Projects/3rd_Party
+## batch: set COMPILER=VS2019
+## batch: set PROGRAM_DIR=D:\Programs
+## batch: set Boost_ROOT=%THIRD_PARTY%\boost\boost_1_72_0\vc142
+## batch: set QT_ROOT=%THIRD_PARTY%/qt
+## batch: call CMakeXCSoar.cmd
