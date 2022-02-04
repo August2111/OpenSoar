e3e54e46b0bf2f6af44b225f296a57272fc9cff3
August2111 <Info@FlapsOnline.de>
2022-02-04 08:34:35 +0100

    WU-6: net/client/WeGlide/UploadIGCFile.cpp add exception handler for UploadFile in case of general exception

--- src/net/client/WeGlide/UploadIGCFile.cpp
+++ src/net/client/WeGlide/UploadIGCFile.cpp
@@ -154,8 +154,16 @@ UploadFile(Path igc_path, User user, Aircraft aircraft,
   catch (const std::exception &e) {
     msg.Format(_T("'%s' - %s"), igc_path.c_str(),
       UTF8ToWideConverter(e.what()).c_str());
-    return flight_data;  // with flight_id = 0!
   }
+  return Flight();  // failure...
+} catch (const std::exception &e) {
+  msg.Format(_T("'%s' - %s"), igc_path.c_str(),
+    UTF8ToWideConverter(e.what()).c_str());
+  return Flight();
+} catch (...) {
+  msg.Format(_T("'%s' - %s"), _("General Exception"), igc_path.c_str());
+  ShowError(std::current_exception(), _T("WeGlide UploadFile"));
+  return Flight();
 }
 
 bool
