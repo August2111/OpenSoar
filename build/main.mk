234663596d6624d1adf36fe99a3477f1982192c8
August2111 <Info@FlapsOnline.de>
2022-02-22 10:54:58 +0100

    WT-3: weglide-task Add basic funtionality (dialogs, widgets commands) for download task from weglide
    WIP: noch sehr überarbeitungsfähig?

--- build/main.mk
+++ build/main.mk
@@ -661,11 +661,14 @@ XCSOAR_SOURCES += \
 	$(SRC)/Dialogs/Contest/WeGlide/FlightPatchDialog.cpp \
 	$(SRC)/Dialogs/Contest/WeGlide/FlightUploadResponse.cpp \
 	$(SRC)/Dialogs/Contest/WeGlide/UserSelection.cpp \
+	$(SRC)/Dialogs/Contest/WeGlide/TaskDownloadDialog.cpp \
 	$(SRC)/net/client/WeGlide/UploadFlight.cpp \
 	$(SRC)/net/client/WeGlide/DeleteFlight.cpp \
 	$(SRC)/net/client/WeGlide/PatchFlight.cpp \
 	$(SRC)/net/client/WeGlide/DeleteIGCFile.cpp \
 	$(SRC)/net/client/WeGlide/PatchIGCFile.cpp \
+	$(SRC)/net/client/WeGlide/UploadFlight.cpp \
+	$(SRC)/net/client/WeGlide/DownloadTask.cpp \
 	$(SRC)/net/client/WeGlide/GetJsonString.cpp \
 	$(SRC)/net/client/WeGlide/GetObjectInfo.cpp \
 	$(SRC)/Tracking/SkyLines/Client.cpp \
