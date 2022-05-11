937b13178866ecb5f104f81f0bb045d3c89be427
August2111 <Info@FlapsOnline.de>
2022-05-11 19:08:15 +0200

    WU-22: WeGlide: add DeleteFlight functionality

--- build/main.mk
+++ build/main.mk
@@ -657,9 +657,12 @@ endif
 XCSOAR_SOURCES += \
 	$(SRC)/net/client/tim/Glue.cpp \
 	$(SRC)/Dialogs/Contest/WeGlide/FlightUploadDialog.cpp \
+	$(SRC)/Dialogs/Contest/WeGlide/FlightDeleteDialog.cpp \
 	$(SRC)/Dialogs/Contest/WeGlide/FlightUploadResponse.cpp \
 	$(SRC)/Dialogs/Contest/WeGlide/UserSelection.cpp \
 	$(SRC)/net/client/WeGlide/UploadFlight.cpp \
+	$(SRC)/net/client/WeGlide/DeleteFlight.cpp \
+	$(SRC)/net/client/WeGlide/DeleteIGCFile.cpp \
 	$(SRC)/net/client/WeGlide/GetJsonString.cpp \
 	$(SRC)/net/client/WeGlide/GetObjectInfo.cpp \
 	$(SRC)/Tracking/SkyLines/Client.cpp \
