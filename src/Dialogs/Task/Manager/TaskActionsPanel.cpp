5418195ec651e9fd3bc75cf90970bcc14fa7a6cf
August2111 <Info@FlapsOnline.de>
2022-12-19 11:10:09 +0100

    WT-6: Dialogs/Task/Manager/TaskActionsPanel - add task selection from an other user

--- src/Dialogs/Task/Manager/TaskActionsPanel.cpp
+++ src/Dialogs/Task/Manager/TaskActionsPanel.cpp
@@ -22,43 +22,40 @@ Copyright_License {
 */
 
 #include "TaskActionsPanel.hpp"
-#include "TaskMiscPanel.hpp"
-#include "TaskListPanel.hpp"
-#include "Internal.hpp"
 #include "../dlgTaskHelpers.hpp"
+#include "Components.hpp"
+#include "Device/Declaration.hpp"
 #include "Dialogs/CoDialog.hpp"
 #include "Dialogs/Error.hpp"
 #include "Dialogs/Message.hpp"
-#include "Components.hpp"
+#include "Engine/Task/Factory/AbstractTaskFactory.hpp"
+#include "Engine/Task/Ordered/OrderedTask.hpp"
+#include "Engine/Waypoint/Waypoints.hpp"
+#include "Interface.hpp"
+#include "Internal.hpp"
+#include "Language/Language.hpp"
 #include "Logger/ExternalLogger.hpp"
 #include "Simulator.hpp"
-#include "Language/Language.hpp"
-#include "Interface.hpp"
-#include "Device/Declaration.hpp"
 #include "Task/ValidationErrorStrings.hpp"
-#include "Engine/Task/Ordered/OrderedTask.hpp"
-#include "Engine/Task/Factory/AbstractTaskFactory.hpp"
-#include "Engine/Waypoint/Waypoints.hpp"
+#include "TaskListPanel.hpp"
+#include "TaskMiscPanel.hpp"
 
 #include "net/client/WeGlide/DownloadTask.hpp"
 
-#include "system/Path.hpp"
 #include "system/FileUtil.hpp"
+#include "system/Path.hpp"
 
-#include "net/client/WeGlide/WeGlideObjects.hpp"
 #include "Dialogs/Contest/WeGlide/TaskDownloadDialog.hpp"
+#include "net/client/WeGlide/WeGlideObjects.hpp"
 
 TaskActionsPanel::TaskActionsPanel(TaskManagerDialog &_dialog,
                                    TaskMiscPanel &_parent,
                                    std::unique_ptr<OrderedTask> &_active_task,
                                    bool *_task_modified) noexcept
-  :RowFormWidget(_dialog.GetLook()),
-   dialog(_dialog), parent(_parent),
-   active_task(_active_task), task_modified(_task_modified) {}
+    : RowFormWidget(_dialog.GetLook()), dialog(_dialog), parent(_parent),
+      active_task(_active_task), task_modified(_task_modified) {}
 
-void
-TaskActionsPanel::SaveTask()
-{
+void TaskActionsPanel::SaveTask() {
   AbstractTaskFactory &factory = active_task->GetFactory();
   factory.UpdateStatsGeometry();
   if (factory.CheckAddFinish())
@@ -74,37 +71,29 @@ TaskActionsPanel::SaveTask()
     DirtyTaskListPanel();
   } else {
     ShowMessageBox(getTaskValidationErrors(errors), _("Task not saved"),
-        MB_ICONEXCLAMATION);
+                   MB_ICONEXCLAMATION);
   }
 }
 
-inline void
-TaskActionsPanel::OnBrowseClicked()
-{
-  parent.SetCurrent(1);
-}
+inline void TaskActionsPanel::OnBrowseClicked() { parent.SetCurrent(1); }
 
-inline void
-TaskActionsPanel::OnNewTaskClicked()
-{
+inline void TaskActionsPanel::OnNewTaskClicked() {
   if ((active_task->TaskSize() < 2) ||
       (ShowMessageBox(_("Create new task?"), _("Task New"),
-                   MB_YESNO|MB_ICONQUESTION) == IDYES)) {
+                      MB_YESNO | MB_ICONQUESTION) == IDYES)) {
     active_task->Clear();
-    active_task->SetFactory(CommonInterface::GetComputerSettings()
-      .task.task_type_default);
+    active_task->SetFactory(
+        CommonInterface::GetComputerSettings().task.task_type_default);
     *task_modified = true;
     dialog.SwitchToPropertiesPanel();
   }
 }
 
-inline void
-TaskActionsPanel::OnDeclareClicked()
-{
+inline void TaskActionsPanel::OnDeclareClicked() {
   const auto errors = active_task->CheckTask();
   if (IsError(errors)) {
     ShowMessageBox(getTaskValidationErrors(errors), _("Declare task"),
-                MB_ICONEXCLAMATION);
+                   MB_ICONEXCLAMATION);
     return;
   }
 
@@ -113,58 +102,50 @@ TaskActionsPanel::OnDeclareClicked()
   ExternalLogger::Declare(decl, way_points.GetHome().get());
 }
 
-inline void
-TaskActionsPanel::OnDownloadClicked() noexcept
-try {
+inline void TaskActionsPanel::OnDownloadClicked() noexcept try {
 
-  auto pilot =  CommonInterface::GetComputerSettings().weglide.pilot; // the preset value
-#ifdef _AUG_MSC  // TODO(August2111)
+  auto pilot =
+      CommonInterface::GetComputerSettings().weglide.pilot; // the preset value
+#ifdef _AUG_MSC                                             // TODO(August2111)
   //  !!! hidden in Linux
   // active_task =
 
   int task =
 #endif
-    TaskDownloadDialog(pilot, _T("Weglide Task"));
+      TaskDownloadDialog(pilot, _T("Weglide Task"));
   //  const auto task_file = WeGlide::DownloadTaskFile(pilot);
-//  // August2111:    if (WeGlide::DownloadTaskDialog()) {
-//  if (File::Exists(task_file))
-//    DirtyTaskListPanel();
+  //  // August2111:    if (WeGlide::DownloadTaskDialog()) {
+  //  if (File::Exists(task_file))
+  //    DirtyTaskListPanel();
 } catch (...) {
   // WIP: nur Platzhalter...
 }
 
-inline void
-TaskActionsPanel::OnUserWeGlideClicked() noexcept
-try {
-  const auto task_file = WeGlide::DownloadTaskFile(WeGlide::User(1752)); // zum Test: Thomas Melde...
-// August2111:    if (WeGlide::DownloadTaskDialog()) {
+inline void TaskActionsPanel::OnUserWeGlideClicked() noexcept try {
+  const auto task_file = WeGlide::DownloadTaskFile(
+      WeGlide::User(1752)); // zum Test: Thomas Melde...
+  // August2111:    if (WeGlide::DownloadTaskDialog()) {
   if (File::Exists(task_file))
     DirtyTaskListPanel();
 } catch (...) {
   // WIP: nur Platzhalter...
 }
 
-void
-TaskActionsPanel::ReClick() noexcept
-{
-  dialog.TaskViewClicked();
-}
+void TaskActionsPanel::ReClick() noexcept { dialog.TaskViewClicked(); }
 
-void
-TaskActionsPanel::Prepare([[maybe_unused]] ContainerWindow &parent,
-                          [[maybe_unused]] const PixelRect &rc) noexcept
-{
+void TaskActionsPanel::Prepare([[maybe_unused]] ContainerWindow &parent,
+                               [[maybe_unused]] const PixelRect &rc) noexcept {
   const auto &settings = CommonInterface::GetComputerSettings();
 
-  AddButton(_("New Task"), [this](){ OnNewTaskClicked(); });
-  AddButton(_("Declare"), [this](){ OnDeclareClicked(); });
-  AddButton(_("Browse"), [this](){ OnBrowseClicked(); });
-  AddButton(_("Save"), [this](){ SaveTask(); });
+  AddButton(_("New Task"), [this]() { OnNewTaskClicked(); });
+  AddButton(_("Declare"), [this]() { OnDeclareClicked(); });
+  AddButton(_("Browse"), [this]() { OnBrowseClicked(); });
+  AddButton(_("Save"), [this]() { SaveTask(); });
 
   if (settings.weglide.pilot.id != 0)
-    AddButton(_("Download WeGlide task"),
-              [this](){ OnDownloadClicked(); });
-  AddButton(_("Download User WeGlide Task"), [this]() { OnUserWeGlideClicked(); });
+    AddButton(_("Download WeGlide task"), [this]() { OnDownloadClicked(); });
+  AddButton(_("Download User WeGlide Task"),
+            [this]() { OnUserWeGlideClicked(); });
 
   if (is_simulator())
     /* cannot communicate with real devices in simulator mode */
