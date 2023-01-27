fcf9a530f55289b4ff1f48e2844e79f3f3c8f245
August2111 <Info@FlapsOnline.de>
2023-01-27 16:29:47 +0100

    compile.mk - revert changes in print output
    but distinguish between CPP and CXX files (with the same compiler)

--- build/compile.mk
+++ build/compile.mk
@@ -106,14 +106,14 @@ $(ABI_OUTPUT_DIR)/%$(OBJ_SUFFIX): %.c | $(ABI_OUTPUT_DIR)/%/../dirstamp $(compil
 # cxx-flags += -v
 
 $(ABI_OUTPUT_DIR)/%$(OBJ_SUFFIX): %.cpp | $(ABI_OUTPUT_DIR)/%/../dirstamp $(compile-depends)
-	@$(NQ)echo "  CXX 1    $@"
+	@$(NQ)echo "  CPP     $@"
 	$(Q)$(WRAPPED_CXX) $< -c -o $@ $(cxx-flags)
 ifeq ($(IWYU),y)
 	$(Q)iwyu $< $(cxx-flags)
 endif
 
 $(ABI_OUTPUT_DIR)/%$(OBJ_SUFFIX): %.cxx | $(ABI_OUTPUT_DIR)/%/../dirstamp $(compile-depends)
-	@$(NQ)echo "  CXX 2    $@"
+	@$(NQ)echo "  CXX     $@"
 	$(Q)$(WRAPPED_CXX) $< -c -o $@ $(cxx-flags)
 ifeq ($(IWYU),y)
 	$(Q)iwyu $< $(cxx-flags)
