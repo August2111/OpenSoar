253634a4adacc25bf3436b210846fdfa751732ae
August2111 <Info@FlapsOnline.de>
2024-09-27 11:40:16 +0200

    [build] install.mk - w/o vali-xcs

--- build/install.mk
+++ build/install.mk
@@ -10,7 +10,7 @@ install-mo: mo
 	done
 
 install-bin: all
-	@$(NQ)echo "  INSTALL   Passiert hier irgend etwas????"
+	@$(NQ)echo "  INSTALL   (August2111) Passiert hier irgend etwas????"
 	install -d -m 0755 $(prefix)/bin
 	install -m 0755 $(TARGET_BIN_DIR)/$(PROGRAM_NAME) $(prefix)/bin
 
