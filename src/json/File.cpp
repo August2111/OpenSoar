db8be2d449a41da6e4707bab4b9af2e17633f67f
August2111 <Info@FlapsOnline.de>
2025-10-23 11:28:00 +0200

    add json/File.*

---
+++ src/json/File.cpp
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright OpenSoar
+// author: Uwe Augustin <Info@OpenSoar.de>
+
+#include "Get.hpp"
+#include "File.hpp"
+#include "system/FileUtil.hpp"
+
+#include <boost/json/serialize.hpp>
+#include <string>
+#include <fstream>
+
+namespace Json {
+  static boost::json::value system_config;
+
+  boost::json::value &Load(Path path) {
+    if (File::Exists(path)) {
+      int buf_size = File::GetSize(path) + 1;
+      char *buffer = new char[buf_size]; // = 4k
+      try {
+        if (File::ReadString(path, buffer, buf_size)) {
+          system_config = boost::json::parse(buffer);
+        }
+      }
+      catch (std::exception &e) {
+      }
+      delete[] buffer;
+    }
+    return system_config;
+  }
+
+  void Save(Path path, const boost::json::value &v)
+  {
+    std::fstream os(path.c_str(), std::fstream::out | std::fstream::binary);
+    PrettyPrint(os, v, 2);
+    os.close();
+  }
+
+  void
+    PrettyPrint(std::ostream &os, boost::json::value const &jv,
+      const size_t indent_size, std::string *indent)
+  {
+    std::string indent_;
+    if (!indent)
+      indent = &indent_;
+    switch (jv.kind())
+    {
+      case boost::json::kind::object:
+      {
+        os << "{" << std::endl;
+        indent->append(indent_size, ' ');
+        auto const &obj = jv.get_object();
+        if (!obj.empty())
+        {
+          auto it = obj.begin();
+          for (;;)
+          {
+            os << *indent << boost::json::serialize(it->key()) << " : ";
+            PrettyPrint(os, it->value(), indent_size, indent);
+            if (++it == obj.end())
+              break;
+            os << "," << std::endl;
+          }
+        }
+        os << std::endl;
+        indent->resize(indent->size() - indent_size);
+        os << *indent << "}";
+        break;
+      }
+
+      case boost::json::kind::array:
+      {
+        os << "[" << std::endl;
+        indent->append(indent_size, ' ');
+        auto const &arr = jv.get_array();
+        if (!arr.empty())
+        {
+          auto it = arr.begin();
+          for (;;)
+          {
+            os << *indent;
+            PrettyPrint(os, *it, indent_size, indent);
+            if (++it == arr.end())
+              break;
+            os << "," << std::endl;
+          }
+        }
+        os << std::endl;
+        indent->resize(indent->size() - indent_size);
+        os << *indent << "]";
+        break;
+      }
+
+      case boost::json::kind::string:
+      {
+        os << boost::json::serialize(jv.get_string());
+        break;
+      }
+
+      case boost::json::kind::uint64:
+        os << jv.get_uint64();
+        break;
+
+      case boost::json::kind::int64:
+        os << jv.get_int64();
+        break;
+
+      case boost::json::kind::double_:
+        os << jv.get_double();
+        break;
+
+      case boost::json::kind::bool_:
+        if (jv.get_bool())
+          os << "true";
+        else
+          os << "false";
+        break;
+
+      case boost::json::kind::null:
+        os << "null";
+        break;
+    }
+
+    if (indent->empty())
+      os << std::endl;
+  }
+
+} // namespace Json
---
+++ src/json/File.hpp
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Copyright OpenSoar
+// author: Uwe Augustin <Info@OpenSoar.de>
+
+#pragma once
+
+#include <boost/json/fwd.hpp>
+#include <string>
+
+class Path;
+
+namespace Json {
+
+boost::json::value &
+Load(Path path);
+
+void 
+Save(Path path, const boost::json::value &v);
+
+void
+PrettyPrint(std::ostream &os, boost::json::value const &jv,
+  const size_t indent_size = 4, std::string *indent = nullptr);
+} // namespace Json
