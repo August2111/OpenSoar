926ebd87c7f124e81fa8135fa53325fb296f75c3
August2111 <Info@FlapsOnline.de>
2026-02-02 11:14:18 +0100

    src/LogFile - fmt is at least fmt8 (Ubuntu 22.04)  missing UniqueFileDescriptor.hxx(  in android)

--- src/LogFile.hpp
+++ src/LogFile.hpp
@@ -5,14 +5,14 @@
 
 #include "util/Compiler.h"
 
-#if 1 // w.o. fmt/core.h not available
+// #if 1 // w.o. fmt/core.h not available
 # include <fmt/core.h>
-# if !defined(FMT_VERSION) || (FMT_VERSION >= 80000 && FMT_VERSION < 90000)
-#   include <fmt/format.h>
-# endif
-#else
+// # if !defined(FMT_VERSION) || (FMT_VERSION >= 80000 && FMT_VERSION < 90000)
+// #   include <fmt/format.h>
+// # endif
+// #else
+// #endif
 # include <fmt/format.h>
-#endif
 
 #include <exception>
 #include <string_view>
@@ -24,14 +24,18 @@ template<typename S, typename... Args>
 void
 LogFmt(const S &format_str, Args&&... args) noexcept
 {
-#if FMT_VERSION >= 90000
-	return LogVFmt(format_str,
+// #if FMT_VERSION >= 90000
+#ifdef _WIN32
+	LogVFmt(format_str,
 		       fmt::make_format_args(args...));
 #else
-	return LogVFmt(fmt::to_string_view(format_str),
-		       fmt::make_args_checked<Args...>(format_str,
-						       args...));
+  // do nothing -> TODO(August2111): !!!!!
 #endif
+// #else
+// 	return LogVFmt(fmt::to_string_view(format_str),
+// 		       fmt::make_args_checked<Args...>(format_str,
+// 						       args...));
+// #endif
 }
 
 /**
