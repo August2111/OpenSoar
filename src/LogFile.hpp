bd9722bb26ac3ae5b5e93b2270601856ea8c2d7c
August2111 <Info@FlapsOnline.de>
2026-02-23 15:13:53 +0100

    fmt: The lowest FMT version used in XCSoar is 10.0, so there is no longer any need to refer to the various code options of the earlier versions.

--- src/LogFile.hpp
+++ src/LogFile.hpp
@@ -5,10 +5,7 @@
 
 #include "util/Compiler.h"
 
-#include <fmt/core.h>
-#if FMT_VERSION >= 80000 && FMT_VERSION < 90000
 #include <fmt/format.h>
-#endif
 
 #include <exception>
 #include <string_view>
@@ -20,14 +17,8 @@ template<typename S, typename... Args>
 void
 LogFmt(const S &format_str, Args&&... args) noexcept
 {
-#if FMT_VERSION >= 90000
 	return LogVFmt(format_str,
 		       fmt::make_format_args(args...));
-#else
-	return LogVFmt(fmt::to_string_view(format_str),
-		       fmt::make_args_checked<Args...>(format_str,
-						       args...));
-#endif
 }
 
 /**
